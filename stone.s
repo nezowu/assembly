1a59248f987dcc52cb07b31045e75b2b527a5cf4
olecya <gxendz@mail.ru>
2016-04-14 10:04:18 +0300

    next_gen

--- stone.s
+++ stone.s
@@ -11,32 +11,33 @@ repr2:
 .section .text
 .global _start
 _start:
-	mov	$318,	%rax
-	mov	$buff,	%rdi
-	mov	$1,	%rsi
-	mov	$0,	%rdx
+	mov	$318,	%rax	/*номер системного вызова getrandom()*/
+	mov	$buff,	%rdi	/*адрес буфера*/
+	mov	$1,	%rsi	/*размер буфера*/
+	mov	$0,	%rdx	/*флаги 0 1 2 или 3*/
 	syscall
 
-	mov	$1,	%r10
-	mov	buff,	%r8
-	mov	$2,	%r9
-	xor	%rsi,	%rsi
-	cmp	repr1,	%r8
-	cmovg	%r10,	%rsi
-	cmp	repr2,	%r8
-	cmovg	%r9,	%rsi
-	mov	%rsi,	ptr
+	mov	$1,	%r10	/*непосредственное значение, флаг 1-scissor(ножницы) в регистр*/
+	mov	buff,	%r8	/*заносим данные из буфера в регистр*/
+	mov	$2,	%r9	/*непосредственное значение, флаг 2-paper(бумага) в регистр*/
+	xor	%rsi,	%rsi	/*записываем флаг 0-stone(камень) в регистр */
+	cmp	repr1,	%r8	/*сравниваем число с диапазоном значений по первую метку*/
+	cmovg	%r10,	%rsi	/*если значение в %r8 больше значения метки то записываем*/
+				/*+в регистр %rsi значение из %r10 тоесть флаг 1-scissor*/
+	cmp	repr2,	%r8	/*сравниваем число с диапазоном значений по вторую метку*/
+	cmovg	%r9,	%rsi	/*меняем значение регистра %rsi*/
+	mov	%rsi,	ptr	/*меняем значение первых 8 байт данных строки ptr*/
 	
-	xor	%rsi,	%rsi
+	xor	%rsi,	%rsi	/*обнуляем значение регистра*/
 
-	xor	%rax,	%rax
-	inc	%rax
-	xor	%rdi,	%rdi
-	inc	%rdi
-	mov	$ptr,	%rsi
-	mov	$9,	%rdx
-	syscall
+	xor	%rax,	%rax	/*получаем нулевое значение в регистре*/
+	inc	%rax		/*увеличиваем значение на 1*/
+	xor	%rdi,	%rdi	/*получаем ноль через побитное, исключающее "или"*/
+	inc	%rdi		/*икримент 0 дает 1*/
+	mov	$ptr,	%rsi	/*заносим адрес в регистр*/
+	mov	$9,	%rdx	/*заносим длину данных расположенных по адресу $ptr*/
+	syscall			/*вызваем прерывание*/
 
-	mov	$60,	%rax
-	mov	$0,	%rsi
-	syscall
+	mov	$60,	%rax	/*номер системного вызова exit*/
+	mov	$0,	%rsi	/*0-без ошибок; коды ошибок /usr/include/asm-generic/errno-base.h*/
+	syscall			/*к сожалению не смогла добится вызова getrandom без ошибки*/
